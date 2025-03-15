1fa53c3c84d394fe9159303b52d28218f9b8d320
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-03-15 15:24:18 +0200

    Add Header Link tags

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -1,28 +1,37 @@
 import { Link } from 'react-router'
 
-export default function Header(){
+export default function Header() {
 
-    return(
+    return (
         <>
-        <header>
-            
-            <h1>
-                <a href="" className="home">Games Play</a>
-            </h1>
-            <nav>
-                <a href="">All Content</a>
-                
-            <div id="user">
-                <a href="">Create Game</a>
-                <a href="">Logout</a>
-            </div>
-                
-            <div id="guest">
-                   <a href="">Login</a>
-                    <a href="">Register</a>
-            </div>
-            </nav>
-        </header>
+            <header>
+
+                <h1><Link className='home' to='/'>Games Play</Link></h1>
+
+                <nav>
+                    <Link to='/games'>
+                        All Games
+                    </Link>
+
+                    <div id="user">
+                        <Link to='/create'>
+                            Create Game
+                        </Link>
+                        <Link to='/logout'>
+                            Logout
+                        </Link>
+                    </div>
+
+                    <div id="guest">
+                        <Link to='/login'>
+                            Login
+                        </Link>
+                        <Link to='/register'>
+                            Register
+                        </Link>
+                    </div>
+                </nav>
+            </header>
         </>
     )
 }
