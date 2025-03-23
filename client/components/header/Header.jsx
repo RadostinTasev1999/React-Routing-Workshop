ff292231f5068bf6a4928b349550358b99790b2d
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-03-23 13:05:55 +0200

    Update Header.jsx
    Add conditional rendering in navigation

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
 import { Link } from 'react-router'
+import { UserContext } from '../../src/contexts/UserContext'
 
 export default function Header() {
+
+    const { email } = useContext(UserContext)
 
     return (
         <>
@@ -13,23 +17,35 @@ export default function Header() {
                         All Games
                     </Link>
 
-                    <div id="user">
-                        <Link to='/create'>
-                            Create Game
-                        </Link>
-                        <Link to='/logout'>
-                            Logout
-                        </Link>
-                    </div>
+                    {
+                        email 
+                          ? (
+                            <div id="user">
+                                <Link to='/create'>
+                                    Create Game
+                                </Link>
+                                <Link to='/logout'>
+                                    Logout
+                                </Link>
+                            </div>
+                        )
+                        :
+                        (
+                                <div id="guest">
+                                    <Link to='/login'>
+                                        Login
+                                    </Link>
+                                    <Link to='/register'>
+                                        Register
+                                    </Link>
+                                </div>
+                        )
+                            
+                    }
 
-                    <div id="guest">
-                        <Link to='/login'>
-                            Login
-                        </Link>
-                        <Link to='/register'>
-                            Register
-                        </Link>
-                    </div>
+                    
+
+                    
                 </nav>
             </header>
         </>
